fadba6961cb682b2f30b6e52a8aa8ef6989c4a1e
thegera4 <84020433+thegera4@users.noreply.github.com>
2023-03-08 21:32:22 -0600

    2nd commit

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -2,7 +2,8 @@ import styled from "styled-components";
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import ChatIcon from '@mui/icons-material/Chat';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
-import { IconButton } from "@mui/material";
+import SearchIcon from '@mui/icons-material/Search';
+import { Button, IconButton } from "@mui/material";
 
 function Sidebar() {
 
@@ -19,6 +20,12 @@ function Sidebar() {
           </IconButton>
         </IconsContainer>
       </Header>
+      <Search>
+        <SearchIcon />
+        <SearchInput placeholder="Search in messages"/>
+      </Search>
+      <SidebarButton>Start a new conversation</SidebarButton>
+      
     </Container>
   )
 }
@@ -30,14 +37,48 @@ const Container = styled.div`
 `;
 
 const Header = styled.div`
-
+  display: flex;
+  position: sticky;
+  top: 0;
+  background-color: white;
+  z-index: 1;
+  justify-content: space-between;
+  align-items: center;
+  padding: 15px;
+  height: 80px;
+  border-bottom: 1px solid whitesmoke;
 `;
 
 const UserAvatar = styled(AccountCircleIcon)`
   color: black;
+  cursor: pointer;
+  :hover {
+    opacity: 0.8;
+  }
 `;
 
 const IconsContainer = styled.div`
 
+`;
+
+const Search = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 20px;
+  border-radius: 2px;
+`;
+
+const SearchInput = styled.input`
+  outline-width: 0;
+  border: none;
+  flex: 1;
+`;
+
+const SidebarButton = styled(Button)`
+  width: 100%;
+  &&& {
+    border-top: 1px solid whitesmoke;
+    border-bottom: 1px solid whitesmoke;
+  }
 `;
 
---
+++ firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAGZOxBK9CoM3qjBvAnj1N-8bT_IISHq9M",
+  authDomain: "web-chat-app-a4050.firebaseapp.com",
+  projectId: "web-chat-app-a4050",
+  storageBucket: "web-chat-app-a4050.appspot.com",
+  messagingSenderId: "994393485573",
+  appId: "1:994393485573:web:949afe6f4396dbbbdd876c"
+};
+
+const app = initializeApp(firebaseConfig);
